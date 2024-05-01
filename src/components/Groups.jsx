78aa588501f00e956ae5f0b2d7653a87611a4df1
umassdgithub <amir.umassd@gmail.com>
2024-05-01 08:10:01 -0400

    update url and config

--- src/components/Groups.jsx
+++ src/components/Groups.jsx
@@ -3,7 +3,7 @@ import React, { useEffect,useState,useRef  } from 'react';
 import DocumentCard from "./UI-components/DocumentCard.jsx";
 import { Container, Row, Col, Form,Button } from 'react-bootstrap';
 // 
-import { API_URL } from '../../config.js';
+//import { API_URL } from '../../config.js';
 
 // I make sure that only 1 group is in all Groups for the papers: only "All"
 export default function Groups() 
